aea68eb7e541e03bed155faa0506b9b9f1c00a08
daviddongguo <david.dong.guo@gmail.com>
2021-02-12 21:23:30 -0500

    Show Top20

--- frontend/top-restaurants-around/src/components/Top20.js
+++ frontend/top-restaurants-around/src/components/Top20.js
@@ -8,13 +8,18 @@ export default function Top20() {
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error!</p>;
 	const list = data.restaurants.map((r) => {
+		const imageUrl =
+			'https://maps.googleapis.com/maps/api/place/photo?key=AIzaSyB2pcmC0RETrG_mCDbSlf58Zz6AWdtsvW0&maxwidth=400&photoreference=' +
+			r.photoreference;
+
 		return (
 			<tr>
+				<img src={imageUrl} alt='' width='125px' height='84px' />
 				<p>Name: {r.name}</p>
-				<p>Description: {r.name}</p>
+				<p>Description: {r.description}</p>
 				<p>Rating: {r.rating}</p>
-				<p>Address: {r.vicinity}</p>
-				<p>Phone Number: {r.vicinity}</p>
+				<p>Address: {r.address}</p>
+				<p>Phone Number: {r.phonenumber}</p>
 			</tr>
 		);
 	});
