44737d3293605287e28f2ae410c7099e425820c2
daviddongguo <david.dong.guo@gmail.com>
2021-02-18 16:08:52 -0500

    Fix server from local to remote in client

--- frontend/top-restaurants-around/src/config/index.js
+++ frontend/top-restaurants-around/src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-	server: process.env.SERVER_URL || 'http://localhost:3003',
+	server: process.env.SERVER_URL || 'http://davidwu.online',
+	// server: process.env.SERVER_URL || 'http://localhost:3003',
 };
 
 export default config;
