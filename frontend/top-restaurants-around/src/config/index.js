f745c82d7435ed8789adaf0179bf9880aa13f20e
daviddongguo <david.dong.guo@gmail.com>
2021-02-19 15:27:47 -0500

    Upgrade config

--- frontend/top-restaurants-around/src/config/index.js
+++ frontend/top-restaurants-around/src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
-	server: process.env.SERVER_URL || 'http://davidwu.online',
-	// server: process.env.SERVER_URL || 'http://localhost:3003',
+	server: 'http://davidwu.online',
+	// server: 'http://localhost:3003',
 };
 
 export default config;
