0af1c3de84f88380ef2da8d5625338a1c53b2aa1
daviddongguo <david.dong.guo@gmail.com>
2021-03-28 10:54:19 -0400

    Upgrade environment variables by adjust .env and config files.

--- backend/config/index.ts
+++ backend/config/index.ts
@@ -4,29 +4,29 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+	// This error should crash whole process
+	throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 export default {
-  port: process.env.PORT || 3001,
+	port: process.env.PORT || 3001,
 
-  google: {
-    key: process.env.GOOGLE_KEY,
-    nearBySearchUrl: process.env.NEAR_BY_SEARCH_URL,
-    placeDetailsUrl: process.env.PLACE_DETAIL_URL,
-    photoUrl: process.env.PHOTO_URL,
-  },
+	google: {
+		// key: process.env.GOOGLE_KEY,
+		nearBySearchUrl: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.GOOGLE_KEY}`,
+		placeDetailsUrl: `https://maps.googleapis.com/maps/api/place/details/json?&key=${process.env.GOOGLE_KEY}`,
+		photoUrl: `https://maps.googleapis.com/maps/api/place/photo?key=${process.env.GOOGLE_KEY}`,
+	},
 
-  logs: {
-    level: process.env.LOG_LEVEL || 'silly',
-  },
+	logs: {
+		level: process.env.LOG_LEVEL || 'silly',
+	},
 
-  company:{
-    name: process.env.COMPANY_NAME,
-    url: process.env.COMPANY_URL,
-    email: process.env.COMPANY_EMAIL,
-    location: process.env.COMPANY_LOCATION,
-    slogans: [process.env.COMPANY_SLOGAN_1, process.env.COMPANY_SLOGAN_2]
-  }
-}
+	company: {
+		name: process.env.COMPANY_NAME,
+		location: process.env.COMPANY_LOCATION,
+		url: process.env.COMPANY_URL,
+		email: process.env.COMPANY_EMAIL,
+		slogans: [process.env.COMPANY_SLOGAN_1, process.env.COMPANY_SLOGAN_2],
+	},
+};
