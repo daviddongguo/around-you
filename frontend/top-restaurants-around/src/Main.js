625d0c21dae3a4b42d57352774d06b687cb5a541
daviddongguo <david.dong.guo@gmail.com>
2021-03-28 11:33:15 -0400

    Stop advertising and contact functions about company on frontend

--- frontend/top-restaurants-around/src/Main.js
+++ frontend/top-restaurants-around/src/Main.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import {Container} from 'reactstrap';
-import Advertising from './components/Advertising';
 import Banner from './components/Banner';
-import Contact from './components/Contact';
 import Top20Show from './components/Top20Show';
 import Top3Show from './components/Top3Show';
 
@@ -11,10 +9,10 @@ class Main extends Component {
 		return (
 			<Container>
 				<Banner />
-				<Advertising />
+				{/* <Advertising /> */}
 				<Top3Show />
 				<Top20Show />
-				<Contact />
+				{/* <Contact /> */}
 			</Container>
 		);
 	}
