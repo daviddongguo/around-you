7bdcea2c9addbbd060cf62f30ef8fe3333d0c5a3
daviddongguo <david.dong.guo@gmail.com>
2021-12-13 21:50:14 -0500

    modify server port

--- config/index.ts
+++ config/index.ts
@@ -1,32 +1,32 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const envFound = dotenv.config();
+const envFound = dotenv.config()
 if (envFound.error) {
-	// This error should crash whole process
-	throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  // This error should crash whole process
+  throw new Error("⚠️  Couldn't find .env file  ⚠️")
 }
 
 export default {
-	port: process.env.PORT || 3001,
+  port: process.env.PORT || 3003,
 
-	google: {
-		// key: process.env.GOOGLE_KEY,
-		nearBySearchUrl: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.GOOGLE_KEY}`,
-		placeDetailsUrl: `https://maps.googleapis.com/maps/api/place/details/json?&key=${process.env.GOOGLE_KEY}`,
-		photoUrl: `https://maps.googleapis.com/maps/api/place/photo?key=${process.env.GOOGLE_KEY}`,
-	},
+  google: {
+    // key: process.env.GOOGLE_KEY,
+    nearBySearchUrl: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.GOOGLE_KEY}`,
+    placeDetailsUrl: `https://maps.googleapis.com/maps/api/place/details/json?&key=${process.env.GOOGLE_KEY}`,
+    photoUrl: `https://maps.googleapis.com/maps/api/place/photo?key=${process.env.GOOGLE_KEY}`
+  },
 
-	logs: {
-		level: process.env.LOG_LEVEL || 'silly',
-	},
+  logs: {
+    level: process.env.LOG_LEVEL || 'silly'
+  },
 
-	company: {
-		name: process.env.COMPANY_NAME,
-		location: process.env.COMPANY_LOCATION,
-		url: process.env.COMPANY_URL,
-		email: process.env.COMPANY_EMAIL,
-		slogans: [process.env.COMPANY_SLOGAN_1, process.env.COMPANY_SLOGAN_2],
-	},
-};
+  company: {
+    name: process.env.COMPANY_NAME,
+    location: process.env.COMPANY_LOCATION,
+    url: process.env.COMPANY_URL,
+    email: process.env.COMPANY_EMAIL,
+    slogans: [process.env.COMPANY_SLOGAN_1, process.env.COMPANY_SLOGAN_2]
+  }
+}
