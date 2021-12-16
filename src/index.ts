104cc9bd2c1f5598a957599695da5614bab22970
daviddongguo <david.dong.guo@gmail.com>
2021-12-15 22:11:57 -0500

    remove cluster

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,4 @@ const start = () => {
   })
 }
 
-import cluster from 'cluster'
-
-import { cpus } from 'os'
-import process from 'process'
-const numCPUs = cpus().length
-
-if (cluster.isPrimary) {
-  console.log(`Primary ${process.pid} is running`)
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`)
-  })
-} else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  start()
-
-  console.log(`Worker ${process.pid} started`)
-}
+start()
